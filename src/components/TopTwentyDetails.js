95f912cf967c2adaf09a1c449d7a0b5fd1b393f2
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-15 18:42:22 +0000

    Created T20 details

---
+++ src/components/TopTwentyDetails.js
@@ -0,0 +1,8 @@
+const TopTwentyDetails = () => {
+
+    return (
+        <h1>These are the T20 Details</h1>
+    )
+}
+
+export default TopTwentyDetails;
--- src/containers/TopTwentyContainer.js
+++ src/containers/TopTwentyContainer.js
@@ -1,10 +1,14 @@
+const { default: TopTwentyDetails } = require("../components/TopTwentyDetails")
 
 
 const TopTwentyContainer = () => {
 
 
     return (
-        <h1>This is the T20 Container</h1>
+        <>
+            <h1>This is the T20 Container</h1>
+            <TopTwentyDetails />
+        </>
 
     )
 }
